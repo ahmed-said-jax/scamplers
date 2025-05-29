fe00f4c6bb1cbd433e206fbe915685e3be83df45
Ahmed Said <ahmed.said@jax.org>
2025-05-29 17:43:35 -0400

    one more time by daft punk

--- db/migrations/0001_institution/up.sql
+++ db/migrations/0001_institution/up.sql
@@ -6,4 +6,4 @@ create table institution (
 );
 
 -- a comment to test CI
--- another comment this time testing for commit verification
+-- another comment this time testing for commit verification again
