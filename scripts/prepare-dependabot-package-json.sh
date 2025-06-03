ae3591a20cf09a61ca6d85f889c6c1c693d5f1ad
Ahmed Said <ahmed.said@jax.org>
2025-06-03 12:13:39 -0400

    dependabot will run against main when this is merged and should work (#83)

--- scripts/prepare-dependabot-package-json.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-jq 'delpaths([["scripts"], ["dependencies", "scamplers-core"]])' typescript/scamplers-frontend/package.json > .github/npm-dependencies/package.json
