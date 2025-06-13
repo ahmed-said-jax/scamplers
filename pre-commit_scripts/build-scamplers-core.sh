894dfa2e3805194a9fee15117d8fa7d327a50a7d
Ahmed Said <ahmed.said@jax.org>
2025-06-13 10:14:15 -0400

    add a nice little source-code check to make sure the source of typescript/scamplers-core is the same as rust/scamplers-core. also change the name of the pre-commit scripts directory

--- pre-commit_scripts/build-scamplers-core.sh
+++ pre-commit_scripts/build-scamplers-core.sh
@@ -2,5 +2,6 @@
 
 cd rust/scamplers-core
 wasm-pack build --release --out-dir ../../typescript/scamplers-core -- --features typescript
+cp -r src ../../typescript/scamplers-core/rust-src
 cd ../../typescript/scamplers-core
 rm .gitignore
