a3a5724dda51c35628ecf60e45faefc406ae658e
Ahmed Said <ahmed.said@jax.org>
2025-06-13 15:06:35 -0400

    add set -e and remove comments

--- pre-commit_scripts/build-scamplers-core.sh
+++ pre-commit_scripts/build-scamplers-core.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-# Build Wasm module
+set -euo pipefail
+
 cd rust/scamplers-core
 wasm-pack build --release --out-dir ../../typescript/scamplers-core -- --features typescript
-
-# Copy the source code for CI/CD to check
 cd ../../typescript/scamplers-core
 rm -r .gitignore rust-src
 cp -r ../../rust/scamplers-core/src rust-src
